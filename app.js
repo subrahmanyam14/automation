55200a5814c22c7d03f1dd07cd05218d3c65fffe
mohan <mohan@incrivelsoft.com>
2024-10-14 10:56:56 +0530

    redeploying project

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ app.use(bodyParser.json());
 app.use("/appointment", appointmentRoutes);
 app.use("/webhook", webhookRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send("Sorry, that route doesn't exist.");
-});
+// app.use((req, res, next) => {
+//   res.status(404).send("Sorry, that route doesn't exist.");
+// });
+
+app.get("/", (req, res) => {
+  return res.send(`Server running upon ${PORT}!`);
+})
 
 mongoose
   .connect(mongoUrl, { useUnifiedTopology: true })
